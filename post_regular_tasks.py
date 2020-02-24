292ec8030b2c15ed2a101724cf545115fcec27cb
Andrew Bosco <abosco@conversantmedia.com>
2020-02-24 11:21:35 -0600

    Merge refactor

--- post_regular_tasks.py
+++ post_regular_tasks.py
@@ -11,24 +11,11 @@ from helpers import RangeDict
 
 class Board:
     def __init__(self, board_name):
-<<<<<<< HEAD
-        base_path = Path(__file__).parent
-        self.cred_file = (base_path / "credentials.json")
-        self.task_file = (base_path / "regular_tasks.json")
-
-        self.get_credentials()
-        self.get_board_id(board_name)
-        self.get_cards()
-        self.get_labels()
-        self.get_lists()
-        self.import_tasks()
-=======
         self._get_board_id(board_name)
         self._get_labels()
         self._get_lists()
         self._get_cards()
         self._import_tasks()
->>>>>>> refactor
 
     def daily_update(self):
         self.archive_cards()
@@ -37,17 +24,7 @@ class Board:
         self.rearrange_cards()
         self.sort_all_lists()
 
-<<<<<<< HEAD
-    def get_credentials(self):
-        with open(self.cred_file, "r") as f:
-            creds = json.load(f)
-        self.key = creds["key"]
-        self.token = creds["token"]
-
-    def get_board_id(self, board_name):
-=======
     def _get_board_id(self, board_name):
->>>>>>> refactor
         url = "https://api.trello.com/1/members/me/boards"
         boards = hlp.request("GET", url)
         board_id = [board["id"] for board in boards if board["name"] == board_name][0]
@@ -69,78 +46,6 @@ class Board:
 
     def _get_lists(self):
         url = f"https://api.trello.com/1/boards/{self.board_id}/lists/"
-<<<<<<< HEAD
-        querystring = {
-            "key": self.key,
-            "token": self.token
-        }
-        r = requests.get(url, params=querystring)
-        lists_ = r.json()
-        lists = {list_["name"]: list_["id"] for list_ in lists_}
-        self.lists = lists
-        self.exempt = [list_id for name, list_id in self.lists.items() if name in EXEMPT]
-        print("Fetched lists")
-
-    def assign_list(self, due_date):
-        now = localize_ts(dt.datetime.now())
-        sunday = (now + dt.timedelta(6 - now.weekday() % 7)).replace(hour=23, minute=59, second=0)
-        diff = int((due_date - now).total_seconds()//3600)
-
-        hours_to_sunday = int((sunday - now).total_seconds()//3600)
-        if hours_to_sunday < 28:
-            hours_to_sunday += 168
-
-        diff_map = RangeDict({
-            range(0,29): "Today",
-            range(29,hours_to_sunday+1): "This Week",
-            range(hours_to_sunday,720): "This Month"
-        })
-        card_list = diff_map.get(diff, "Beyond")
-        return card_list
-    
-    def create_card(self, task):
-        due_date = self.assign_due_date(task["date_info"])
-        card_list = self.assign_list(due_date)
-        list_id = self.lists[card_list]
-        label_ids = [self.labels[label] for label in task["labels"]]
-        card_name = task["name"]
-        body = {
-            "last_complete": task['date_info']['last_complete'],
-            "time_estimate": task['time_estimate']
-        }
-        
-        url = "https://api.trello.com/1/cards"
-        querystring = {
-            "idList": list_id,
-            "name": card_name,
-            "idLabels": label_ids,
-            "due": due_date,
-            "desc": format_desc(body),
-            "key": self.key,
-            "token": self.token
-        }
-        requests.post(url, params=querystring)
-        print(f"Posted card: {card_name} to list {card_list} (due {due_date.date()})")
-
-    def assign_due_date(self, date_info):
-        if date_info["last_complete"]:
-            base = dt.datetime.strptime(date_info["last_complete"], "%Y-%m-%d")
-        else:
-            base = dt.datetime.today()
-        base = base.replace(hour=23,minute=59,second=0)
-        due_date = base + dt.timedelta(date_info["delta"])
-
-        next_sunday = lambda x: x + dt.timedelta(6 - x.weekday() % 7)
-        if date_info["advance"]:
-            due_date = next_sunday(due_date)
-
-        return localize_ts(due_date)
-
-    def import_tasks(self):
-        with open(self.task_file, "r") as f:
-            self.tasks = json.load(f)
-        self.task_names = [task["name"] for task in self.tasks]
-=======
         lists_ = hlp.request("GET", url)
 
         self.lists = {}
@@ -161,125 +66,16 @@ class Board:
         task_output = hlp.format_tasks(self.tasks)
         with open(TASK_FILE, "w") as f:
             json.dump(task_output, f, default=hlp.date_handler)
->>>>>>> refactor
 
     def post_tasks(self):
         for task in self.tasks:
             if task.name not in self.card_names:
                 task.create_card()
             else:
-<<<<<<< HEAD
-                print(f"Card skipped: {task['name']}")
-
-    def update_task_file(self):
-        with open(self.task_file, "w") as f:
-            json.dump(self.tasks, f)
-
-    def log_date(self, list_cards):
-        for card in list_cards:
-            due_date = card["due"].strftime("%Y-%m-%d")
-            for i, task in enumerate(self.tasks):
-                if card["name"] == task["name"]:
-                    self.tasks[i]["date_info"]["last_complete"] = due_date
-
-    def archive_cards(self, list_name="Done"):
-        list_id = self.lists[list_name]
-        list_cards = [card for card in self.cards if card["list"] == list_id]
-        
-        url = f"https://api.trello.com/1/lists/{list_id}/archiveAllCards"
-        querystring = {
-            "key": self.key,
-            "token": self.token
-        }
-        requests.post(url, params=querystring)
-        
-        card_names = [card["name"] for card in list_cards]
-        print(f"All cards archived in list {list_name}: {card_names}")
-        self.log_date(list_cards)
-        self.update_task_file()
-        self.get_cards()
-
-    def sort_list(self, card_list):
-        prefer_order = lambda x: (
-            self.label_names[x["labels"][0]],
-            x["due"],
-            x["name"])
-        card_list = sorted(card_list, key=prefer_order)
-
-        for rank, card in enumerate(card_list, start=1):
-            url = f"https://api.trello.com/1/cards/{card['id']}"
-            querystring = {"key": self.key, "token": self.token, "pos": rank}
-            requests.put(url, querystring)
-
-    def get_list_cards(self, list_id):
-        url = f'https://api.trello.com/1/lists/{list_id}/cards'
-        querystring = {"key": self.key, "token": self.token, "fields": ["id","name","desc","labels","due"]}
-        list_cards = requests.get(url, params=querystring).json()
-        return list_cards
-
-    def get_stats(self, card):
-        stat_split = card["desc"].split("#Stats\n")[1].replace("**","").split("\n")
-        for stat in stat_split:
-            key, val = stat.split(": ")
-            val = val.strip()
-            val = int(val) if val.isnumeric() else val
-            card[key] = val
-        return card
-
-    def list_time_summary(self, list_id):
-        card_list = self.get_list_cards(list_id)
-        by_label = defaultdict(int)
-        for card in card_list:
-            card = self.get_stats(card)
-            label = card["labels"][0]["name"]
-            by_label[label] += card["Time estimate"]
-        return by_label
-
-    def list_time_sum(self, list_id, breakout=False):
-        card_list = self.get_list_cards(list_id)
-        sprint_time = 0
-        for card in card_list:
-            card = self.get_stats(card)
-            sprint_time += card["Time estimate"]
-        if breakout:
-            card_list = sorted(card_list, key=lambda x: x["Time estimate"], reverse=True)
-            for card in card_list:
-                print(card["name"], card["Time estimate"])
-        return sprint_time
-
-    def rearrange_cards(self):
-        self.get_cards()
-        for card in self.cards:
-            card_list = card["list"]
-            new_list = self.lists[self.assign_list(card["due"])]
-            if card_list != new_list and card_list not in self.exempt:
-                self.move_card(card["id"], new_list)
-                print(f"Moved card {card['name']} to {new_list} (due {card['due']})")
-
-    def move_card(self, card, new_list):
-        url = f"https://api.trello.com/1/cards/{card}"
-        querystring = {
-            "key": self.key,
-            "token": self.token,
-            "idList": new_list
-        }
-        requests.put(url, params=querystring)
-
-
-    def sort_all_lists(self):
-        self.get_cards()
-        groups = defaultdict(list)
-        for card in self.cards:
-            groups[card["list"]].append(card)
-        for card_list in groups.values():
-            self.sort_list(card_list)
-
-=======
                 print(f"Card skipped: {task.name}")
         inbox = self.lists["Inbox"]
         inbox.get_list_cards()
     
->>>>>>> refactor
     def add_task(self):
         name = input("Enter task name: ")
         labels = input("Enter label names, separated by comma: ").split(",")
